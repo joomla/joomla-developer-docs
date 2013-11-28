1b3222e9014b4b411901f2b2c787829d079ae348
Andrew Eddie <mamboblue@gmail.com>
2013-11-28 19:56:03 +1000

    Fix up code style.

--- js/controllers.js
+++ js/controllers.js
@@ -2,28 +2,28 @@
 
 /* Controllers */
 function DocController($scope, $routeParams, $http, $route, $location, $anchorScroll, $window) {
-	if ($routeParams.lang == '' || typeof($routeParams.lang) == 'undefined') {
+	if($routeParams.lang == '' || typeof($routeParams.lang) == 'undefined') {
 		$routeParams.lang = 'en';
 	}
-	if (typeof($routeParams.part1) == 'undefined') {
+	if(typeof($routeParams.part1) == 'undefined') {
 		$routeParams.part1 = 'home.md';
 	}
 
 	var page = $routeParams.lang + '/' + $routeParams.part1;
 
-	if (typeof($routeParams.part2) != 'undefined') {
+	if(typeof($routeParams.part2) != 'undefined') {
 		page += '/' + $routeParams.part2;
 	}
 
-	if (typeof($routeParams.part3) != 'undefined') {
+	if(typeof($routeParams.part3) != 'undefined') {
 		page += '/' + $routeParams.part3;
 	}
 
-	if (typeof($routeParams.part4) != 'undefined') {
+	if(typeof($routeParams.part4) != 'undefined') {
 		page += '/' + $routeParams.part4;
 	}
 
-	$http.get('docs/' + page).success(function (data) {
+	$http.get('docs/' + page).success(function(data) {
 		$scope.pageContent = data;
 		/* point the edit to the original master, then you are prompted to fork if you don't have write access */
 		$scope.sourceEdit = 'https://github.com/joomla/joomla-developer-docs/edit/master/docs/' + page;
@@ -43,7 +43,7 @@ function MenuController($scope, $http, $route, $templateCache) {
 
 	// get the commits that affect the docs path only
 	$http({method: $scope.method, url: $scope.commitUrl, cache: $templateCache}).
-		success(function (data, status) {
+		success(function(data, status) {
 			$scope.status = status;
 			$scope.commit = data;
 			$scope.commitSha = [
@@ -76,22 +76,22 @@ function MenuController($scope, $http, $route, $templateCache) {
 			// Variable used to build the HTML tree
 			var menuTreeHTML = "";
 			// foreach loop to get the sha of the latest commit for the tree
-			angular.forEach($scope.commit, function (value, key) {
-				if ($scope.count > 0) return;
+			angular.forEach($scope.commit, function(value, key) {
+				if($scope.count > 0) return;
 				$scope.commitSha.push({sha: value});
 				// Reference the official Joomla repo
 				$scope.treeUrl = 'https://api.github.com/repos/joomla/joomla-developer-docs/git/trees/' + value.sha + '?recursive=1';
 				// get the tree details - unfortunately it is a flat tree, not nested with children, so we have to create the hierachy
 				$http({method: $scope.method, url: $scope.treeUrl, cache: $templateCache}).
-					success(function (data, status) {
+					success(function(data, status) {
 						$scope.status = status;
 						$scope.treeDetails = data;
 						// only store the paths that begin with "docs/**/cms/" or "docs/**/framework" and ends with "md"
-						angular.forEach($scope.treeDetails.tree, function (value, key) {
-							if (value.path.match(markdownCMSFiles) != null | value.path.match(markdownFrameworkFiles) != null) {
+						angular.forEach($scope.treeDetails.tree, function(value, key) {
+							if(value.path.match(markdownCMSFiles) != null | value.path.match(markdownFrameworkFiles) != null) {
 								// Get a list of paths
 								var splitRoute = value.path.match(separatePath);
-								if (splitRoute != null) {
+								if(splitRoute != null) {
 									treeJSON.name = splitRoute[2];
 									treeJSON.path = splitRoute[1].replace(/^docs\//, "");
 									treeJSON.depth = treeJSON.path.split("/").length;
@@ -103,32 +103,32 @@ function MenuController($scope, $http, $route, $templateCache) {
 							}
 						});
 						// build the html menu tree
-						angular.forEach($scope.treeJSONarray, function (value, key) {
+						angular.forEach($scope.treeJSONarray, function(value, key) {
 							// insert home to the top of the tree
-							if (key == 1) {
+							if(key == 1) {
 								menuTreeHTML = '<ul><li><a href="/#/en/home.md" title="Home">Home</a></li>';
 							}
-							else if (key == 2) {
+							else if(key == 2) {
 								lastFolder = $scope.treeJSONarray[key].path.split("/");
 								menuTreeHTML += '<li>' + lastFolder[lastFolder.length - 1].toUpperCase() + '</a><ul>';
 								menuTreeHTML += '<li><a href="/#/' + $scope.treeJSONarray[key].path + '/' + $scope.treeJSONarray[key].name + '" title="' + $scope.treeJSONarray[key].name + '">' + $scope.treeJSONarray[key].name.replace(/\.md$/, "") + '</a></li>';
 							}
 							else {
-								if ($scope.treeJSONarray[key].depth > lastDepth) {
+								if($scope.treeJSONarray[key].depth > lastDepth) {
 									lastFolder = $scope.treeJSONarray[key].path.split("/");
 									menuTreeHTML += '<li>' + lastFolder[$scope.treeJSONarray[key].depth - 1].toUpperCase() + '</a><ul>';
 									menuTreeHTML += '<li><a href="/#/' + $scope.treeJSONarray[key].path + '/' + $scope.treeJSONarray[key].name + '" title="' + $scope.treeJSONarray[key].name + '">' + $scope.treeJSONarray[key].name.replace(/\.md$/, "") + '</a></li>';
 								}
-								if ($scope.treeJSONarray[key].depth == lastDepth) {
+								if($scope.treeJSONarray[key].depth == lastDepth) {
 									lastFolder = $scope.treeJSONarray[key].path.split("/");
 									menuTreeHTML += '<li><a href="/#/' + $scope.treeJSONarray[key].path + '/' + $scope.treeJSONarray[key].name + '" title="' + $scope.treeJSONarray[key].name + '">' + $scope.treeJSONarray[key].name.replace(/\.md$/, "") + '</a>';
 									menuTreeHTML += '</li>';
 								}
-								else if ($scope.treeJSONarray[key].depth < lastDepth) {
+								else if($scope.treeJSONarray[key].depth < lastDepth) {
 									menuTreeHTML += '</ul>';
-									if ($scope.treeJSONarray[key].depth == 2) {
+									if($scope.treeJSONarray[key].depth == 2) {
 										lastFolder = $scope.treeJSONarray[key].path.split("/");
-										if (lastFolder[1] != $scope.treeJSONarray[key - 1].path.split("/")[1]) {
+										if(lastFolder[1] != $scope.treeJSONarray[key - 1].path.split("/")[1]) {
 											menuTreeHTML += '</ul>';
 											menuTreeHTML += '<li>' + lastFolder[$scope.treeJSONarray[key].depth - 1].toUpperCase() + '<ul>';
 										}
@@ -137,21 +137,21 @@ function MenuController($scope, $http, $route, $templateCache) {
 									}
 								}
 							}
-							if (key == $scope.treeJSONarray.length) {
+							if(key == $scope.treeJSONarray.length) {
 								menuTreeHTML += '</ul></ul>';
 							}
 							lastDepth = $scope.treeJSONarray[key].depth;
 						});
 						$scope.treeJSONarray = menuTreeHTML;
 					}).
-					error(function (data, status) {
+					error(function(data, status) {
 						$scope.treeDetails = data || "Request failed";
 						$scope.status = status;
 					});
 				$scope.count++;
 			});
 		}).
-		error(function (data, status) {
+		error(function(data, status) {
 			$scope.commit = data || "Request failed";
 			$scope.status = status;
 		});
